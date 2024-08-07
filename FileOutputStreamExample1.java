f5034209e18c6fb7af463962719fb7c3be34b2f8
problemsolver511 <problemsolver592@gmail.com>
2024-08-07 08:50:56 +0530

    fileoutputstream ex

---
+++ FileOutputStreamExample1.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+class FileOutputStreamExample1{
+
+
+public static void main(String[] arg){
+
+
+try{
+
+FileOutputStream  fout = new FileOutputStream("E:\\Sandbox\\test.txt");
+
+fout.write(90);
+fout.close();
+System.out.println("Success");
+
+}
+catch(Exception e){
+System.out.println(e);
+}
+
+
+}
+}
