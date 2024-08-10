d37d80464a712f32bef09bd1c3d76564ab885169
problemsolver511 <manirathnam501@gmail.com>
2024-08-10 20:33:34 +0530

    output stream ex

---
+++ FileOutputStreamExample2.java
@@ -0,0 +1,13 @@
+class FileOutputStreamExample {  
+    public static void main(String args[]){    
+          
+           try{    
+             FileOutputStream fout=new FileOutputStream("D:\\testout.txt");    
+             fout.write(65);    
+             fout.close();    
+             System.out.println("success...");    
+            }catch(Exception e){System.out.println(e);}    
+      
+      }    
+
+ }  
