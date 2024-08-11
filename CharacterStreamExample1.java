b6e8a4201d33bcf35607607e055f09b879e18bae
problemsolver511 <manirathnam501@gmail.com>
2024-08-11 10:23:16 +0530

    fileoutputstream ex

---
+++ CharacterStreamExample1.java
@@ -0,0 +1,44 @@
+import java.io.*;
+
+class CharacterStreamExample1{
+	
+
+	public static void main(String[] arg) throws IOException{
+
+
+    char[] array = {'H','e','l','l','o'};
+
+		try{
+            
+			CharArrayReader  cis = new CharArrayReader(array);
+            System.out.println("The character reader from reader");
+            int charRead;
+
+
+while ((charRead=cis.read())!=-1) {  
+                System.out.print((char)charRead+",");  
+            }  
+            
+		}
+
+/*
+            while ((charRead=cis.read())!=-1){
+            	System.out.print((char)charRead +',');
+            }
+        }*/
+       
+
+/* try {  
+            CharArrayReader reader = new CharArrayReader(array);  
+            System.out.print("The characters read from the reader:");  
+            int charRead;  
+*/
+            
+	
+		catch(Exception io){
+
+            	System.out.println(io.getMessage());
+            }
+     
+	}
+}
--- FileOutputStreamExample2.java
+++ FileOutputStreamExample2.java
@@ -2,7 +2,7 @@ class FileOutputStreamExample {
     public static void main(String args[]){    
           
            try{    
-             FileOutputStream fout=new FileOutputStream("D:\\testout.txt");    
+             FileOutputStream fout = new FileOutputStream("D:\\testout.txt");    
              fout.write(65);    
              fout.close();    
              System.out.println("success...");    
