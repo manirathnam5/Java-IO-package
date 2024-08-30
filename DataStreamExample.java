bdb2e1b3ec71b4873e1e95318b7c505777425aff
manirathnam5 <manirathnam501@gmail.com>
2024-08-30 22:15:58 +0530

    code refactor

--- DataStreamExample.java
+++ DataStreamExample.java
@@ -1,11 +1,10 @@
 public class DataStreamExample {  
      public static void main(String args[]){    
-          
+
           try{    
             FileInputStream fin = new FileInputStream("E:\\testout.txt");    
             int i=fin.read();  
             System.out.print((char)i);    
-  
             fin.close();    
           }catch(Exception e){System.out.println(e);}    
          }    
